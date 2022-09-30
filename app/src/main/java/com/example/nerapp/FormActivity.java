1da31fe00b09093193cc27dc27727d2ef004d71d
Sarthak <sarthaksharma618@gmail.com>
2022-09-30 18:51:58 +0530

    New Update for the androd application code.

--- app/src/main/java/com/example/nerapp/FormActivity.java
+++ app/src/main/java/com/example/nerapp/FormActivity.java
@@ -4,25 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telecom.Connection;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class FormActivity extends AppCompatActivity {
+    HttpURLConnection httpURLConnection = null;
+    BufferedReader bufferedReader = null;
+    String json;
+    StringBuilder stringBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
-
-
         TextView t6 = (TextView) findViewById(R.id.t6);
         TextView t7 = (TextView) findViewById(R.id.t7);
         TextView t8 = (TextView) findViewById(R.id.t8);
         TextView t9 = (TextView) findViewById(R.id.t9);
         TextView t10 = (TextView) findViewById(R.id.t10);
 
-        new NewActivePage();
+        NewActivePage newActivePage = new NewActivePage();
+        newActivePage.execute();
+    }
+    private class NewActivePage extends AsyncTask<Void, Void, String>{
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL("192.168.1.6/name");
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                bufferedReader = new BufferedReader(new InputStreamReader(
+                        httpURLConnection.getInputStream()
+                ));
+                stringBuilder = new StringBuilder();
+
+                while ((json = bufferedReader.readLine()) != null){
+                    stringBuilder.append(json);
+                }
+                System.out.println("My name is : "+stringBuilder);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
